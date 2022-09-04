712a88cd658cb28851409f9a855172e03782eb96
Artur <89503357000@ya.ru>
2022-09-05 01:26:37 +0600

    main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
-// которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию 
+// элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -9,35 +9,64 @@
 // 2 3 5 9
 // 2 4 4 8
 
-Console.WriteLine("Введи число");
-int a = Convert.ToInt32(Console.ReadLine());;
-Console.WriteLine("Введи число");
-int b = Convert.ToInt32(Console.ReadLine());;
-int[,] numbers = new int[a, b];
-int m = 0;
-int n = 0;
-ArrayDouble(numbers);
-WriteArray(numbers);
+int columnArray = ReadInt("Введите количество столбцов: ");
+int rowArray = ReadInt("Введите количество строк: ");
 
-void ArrayDouble(int[,] array)
+int[,] matrix = new int[rowArray, columnArray];
+FillArrayRandom(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+int[,] matrixSort = SortArray(matrix);
+PrintArray(matrixSort);
+
+int[,] SortArray(int[,] array)
 {
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(0, 10);
+        int temp;
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int b = j + 1; b < array.GetLength(1); b++)
+            {
+                if (array[i, j] > array[i, b])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, b];
+                    array[i, b] = temp;
+                }
+            }
+        }
+
     }
-  }
+    return matrix;
 }
-void WriteArray (int[,] array)
+
+int ReadInt (string message)
 {
-for (int i = 0; i < m; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
-        int Number = array[i, j];
-        Console.Write(Number + "\t");
-      }
-      Console.WriteLine();
-  }
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+void FillArrayRandom (int[,] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t ");
+        }
+        Console.WriteLine();
+    }
 }
